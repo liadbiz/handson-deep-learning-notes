afb0df7902423acd3bbeb115c94ef2d269f68b12
liadbiz <2712079998@qq.com>
2019-07-04 15:34:55 +0800

    init

---
+++ code/LeNet5.py
@@ -0,0 +1,11 @@
+import tensorflow
+import matplotlib
+import matplotlib.pyplot as plt
+plt.switch_backend('TkAgg')
+import numpy as np
+
+print("Tensorflow Imported")
+plt.plot(np.arange(100))
+plt.show()
+# plt.savefig('np.png')
+print("hello world")
